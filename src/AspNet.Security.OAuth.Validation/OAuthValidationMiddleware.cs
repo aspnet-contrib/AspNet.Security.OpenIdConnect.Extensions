817fd0094b26ce81ef61d4459459120c3d1695cb
Kévin Chalet <kevinchalet@gmail.com>
2015-11-29 23:07:27 +0100

    Add a simple OAuth2 validation middleware for AspNet.Security.OpenIdConnect.Server

---
+++ src/AspNet.Security.OAuth.Validation/OAuthValidationMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNet.Authentication;
+using Microsoft.AspNet.DataProtection;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Internal;
+using Microsoft.Extensions.Logging;
+
+namespace AspNet.Security.OAuth.Validation {
+    public class OAuthValidationMiddleware : AuthenticationMiddleware<OAuthValidationOptions> {
+        public OAuthValidationMiddleware(
+            [NotNull] RequestDelegate next,
+            [NotNull] OAuthValidationOptions options,
+            [NotNull] ILoggerFactory loggerFactory,
+            [NotNull] UrlEncoder encoder,
+            [NotNull] IDataProtectionProvider dataProtectionProvider)
+            : base(next, options, loggerFactory, encoder) {
+            if (options.TicketFormat == null) {
+                // Note: the purposes of the default ticket
+                // format must match the values used by ASOS.
+                options.TicketFormat = new TicketDataFormat(
+                    dataProtectionProvider.CreateProtector(
+                        "AspNet.Security.OpenIdConnect.Server.OpenIdConnectServerMiddleware",
+                        "oidc-server", "Access_Token", "v1"));
+            }
+        }
+
+        protected override AuthenticationHandler<OAuthValidationOptions> CreateHandler() {
+            return new OAuthValidationHandler();
+        }
+    }
+}
---
+++ test/AspNet.Security.OAuth.Validation.Tests/OAuthValidationMiddlewareTests.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AspNet.Security.OpenIdConnect.Extensions;
+using Microsoft.AspNet.Authentication;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Http.Authentication;
+using Microsoft.AspNet.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace AspNet.Security.OAuth.Validation.Tests {
+    public class OAuthValidationMiddlewareTests {
+        [Fact]
+        public async Task InvalidTokenCausesInvalidAuthentication() {
+            // Arrange
+            var server = CreateResourceServer();
+
+            var client = server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "invalid-token");
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ValidTokenAllowsSuccessfulAuthentication() {
+            // Arrange
+            var server = CreateResourceServer();
+
+            var client = server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "token-1");
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("Fabrikam", await response.Content.ReadAsStringAsync());
+        }
+
+        [Fact]
+        public async Task MissingAudienceCausesInvalidAuthentication() {
+            // Arrange
+            var server = CreateResourceServer(options => {
+                options.Audience = "http://www.fabrikam.com/";
+            });
+
+            var client = server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "token-1");
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task InvalidAudienceCausesInvalidAuthentication() {
+            // Arrange
+            var server = CreateResourceServer(options => {
+                options.Audience = "http://www.fabrikam.com/";
+            });
+
+            var client = server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "token-2");
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ValidAudienceAllowsSuccessfulAuthentication() {
+            // Arrange
+            var server = CreateResourceServer(options => {
+                options.Audience = "http://www.fabrikam.com/";
+            });
+
+            var client = server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "token-3");
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("Fabrikam", await response.Content.ReadAsStringAsync());
+        }
+
+        [Fact]
+        public async Task ExpiredTicketCausesInvalidAuthentication() {
+            // Arrange
+            var server = CreateResourceServer();
+
+            var client = server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "token-4");
+
+            // Act
+            var response = await client.SendAsync(request);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        private static TestServer CreateResourceServer(Action<OAuthValidationOptions> configuration = null) {
+            var builder = TestServer.CreateBuilder();
+
+            var format = new Mock<ISecureDataFormat<AuthenticationTicket>>();
+
+            format.Setup(mock => mock.Unprotect(It.Is<string>(token => token == "invalid-token")))
+                  .Returns(value: null);
+
+            format.Setup(mock => mock.Unprotect(It.Is<string>(token => token == "token-1")))
+                  .Returns(delegate {
+                      var identity = new ClaimsIdentity(OAuthValidationDefaults.AuthenticationScheme);
+                      identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "Fabrikam"));
+
+                      var properties = new AuthenticationProperties();
+
+                      return new AuthenticationTicket(new ClaimsPrincipal(identity),
+                          properties, OAuthValidationDefaults.AuthenticationScheme);
+                  });
+
+            format.Setup(mock => mock.Unprotect(It.Is<string>(token => token == "token-2")))
+                  .Returns(delegate {
+                      var identity = new ClaimsIdentity(OAuthValidationDefaults.AuthenticationScheme);
+                      identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "Fabrikam"));
+
+                      var properties = new AuthenticationProperties();
+                      properties.SetAudiences(new[] { "http://www.google.com/" });
+
+                      return new AuthenticationTicket(new ClaimsPrincipal(identity),
+                          properties, OAuthValidationDefaults.AuthenticationScheme);
+                  });
+
+            format.Setup(mock => mock.Unprotect(It.Is<string>(token => token == "token-3")))
+                  .Returns(delegate {
+                      var identity = new ClaimsIdentity(OAuthValidationDefaults.AuthenticationScheme);
+                      identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "Fabrikam"));
+
+                      var properties = new AuthenticationProperties();
+                      properties.SetAudiences(new[] {
+                          "http://www.google.com/",
+                          "http://www.fabrikam.com/"
+                      });
+
+                      return new AuthenticationTicket(new ClaimsPrincipal(identity),
+                          properties, OAuthValidationDefaults.AuthenticationScheme);
+                  });
+
+            format.Setup(mock => mock.Unprotect(It.Is<string>(token => token == "token-4")))
+                  .Returns(delegate {
+                      var identity = new ClaimsIdentity(OAuthValidationDefaults.AuthenticationScheme);
+                      identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "Fabrikam"));
+
+                      var properties = new AuthenticationProperties();
+                      properties.ExpiresUtc = DateTimeOffset.UtcNow - TimeSpan.FromDays(1);
+
+                      return new AuthenticationTicket(new ClaimsPrincipal(identity),
+                          properties, OAuthValidationDefaults.AuthenticationScheme);
+                  });
+
+            builder.UseEnvironment("Testing");
+
+            builder.UseServices(services => {
+                services.AddAuthentication();
+            });
+
+            builder.UseStartup(app => {
+                app.UseOAuthValidation(options => {
+                    options.AutomaticAuthenticate = true;
+                    options.AutomaticChallenge = true;
+                    options.TicketFormat = format.Object;
+
+                    // Run the configuration delegate
+                    // registered by the unit tests.
+                    configuration?.Invoke(options);
+                });
+
+                app.Run(context => {
+                    if (!context.User.Identities.Any(identity => identity.IsAuthenticated)) {
+                        return context.Authentication.ChallengeAsync();
+                    }
+
+                    return context.Response.WriteAsync(context.User.GetClaim(ClaimTypes.NameIdentifier));
+                });
+            });
+
+            return new TestServer(builder);
+        }
+    }
+}
